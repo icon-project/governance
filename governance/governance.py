c0ef8477b0b364fd153c25b739529804ae943615
Jaechang Namgoong <jnamgoong@icon.foundation>
2018-06-28 09:58:22 +0900

    Relocate codes

--- governance/governance.py
+++ governance/governance.py
@@ -19,6 +19,7 @@ class Governance(IconScoreBase):
 
     _SCORE_STATUS = 'score_status'
 
+    # TODO: replace with real func
     _MAP_ADDRESS = {
         '0xe0f6dc6607aa9b5550cd1e6d57549f67fe9718654cde15258922d0f88ff58b27': 'cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32',
         '0xe22222222222222250cd1e6d57549f67fe9718654cde15258922d0f88ff58b27': 'cx222222222f5b45bfaea8cff1d8232fbb6122ec32',
@@ -38,29 +39,11 @@ class Governance(IconScoreBase):
     def on_update(self) -> None:
         super().on_update()
 
-    @external(readonly=True)
-    def getScoreStatus(self, address: Address):  # TODO: add dict type hint
-        tx_hash = self._MAP_TXHASH[str(address)]  # TODO: replace with real func
-        if tx_hash is None:
-            self.revert('SCORE not found')
-        result = {}
-        _current = self._get_current_status(address)
-        count1, status = self._fill_status(_current)
-        if count1 > 0:
-            result[CURRENT] = status
-        _next = self._get_next_status(address)
-        count2, status = self._fill_status(_next)
-        if count2 > 0:
-            result[NEXT] = status
-        if count1 + count2 == 0:
-            # there is no status information, build initial status
-            status = {
-                STATUS: STATUS_PENDING,
-                DEPLOY_TXHASH: tx_hash
-            }
-            result[NEXT] = status
-            # self._save_status(_next, status)  # put is not allowed here!
-        return result
+    def _get_current_status(self, score_address: Address):
+        return self._score_status[score_address][CURRENT]
+
+    def _get_next_status(self, score_address: Address):
+        return self._score_status[score_address][NEXT]
 
     @staticmethod
     def _fill_status(db: DictDB):
@@ -87,6 +70,30 @@ class Governance(IconScoreBase):
             value = db[key]
             if value:
                 del db[key]
+
+    @external(readonly=True)
+    def getScoreStatus(self, address: Address) -> dict:
+        tx_hash = self._MAP_TXHASH[str(address)]  # TODO: replace with real func
+        if tx_hash is None:
+            self.revert('SCORE not found')
+        result = {}
+        _current = self._get_current_status(address)
+        count1, status = self._fill_status(_current)
+        if count1 > 0:
+            result[CURRENT] = status
+        _next = self._get_next_status(address)
+        count2, status = self._fill_status(_next)
+        if count2 > 0:
+            result[NEXT] = status
+        if count1 + count2 == 0:
+            # there is no status information, build initial status
+            status = {
+                STATUS: STATUS_PENDING,
+                DEPLOY_TXHASH: tx_hash
+            }
+            result[NEXT] = status
+            # self._save_status(_next, status)  # put is not allowed here!
+        return result
 
     @external
     def acceptScore(self, txHash: str):
@@ -152,9 +159,3 @@ class Governance(IconScoreBase):
     @external
     def removeAuditor(self, address: Address):
         pass
-
-    def _get_current_status(self, score_address: Address):
-        return self._score_status[score_address][CURRENT]
-
-    def _get_next_status(self, score_address: Address):
-        return self._score_status[score_address][NEXT]
