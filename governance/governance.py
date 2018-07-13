bcfb57b62c44221db4878dcbf09103f02af2bf97
leeheonseung <mycom22@icon.Foundation>
2018-07-13 10:32:56 +0900

    fix feedback (#178)

--- governance/governance.py
+++ governance/governance.py
@@ -15,72 +15,43 @@ STATUS_ACTIVE = 'active'
 STATUS_INACTIVE = 'inactive'
 STATUS_REJECTED = 'rejected'
 
-STEP_TYPE_DEFAULT = 'default'
-STEP_TYPE_CONTRACT_CALL = 'contractCall'
-STEP_TYPE_CONTRACT_CREATE = 'contractCreate'
-STEP_TYPE_CONTRACT_UPDATE = 'contractUpdate'
-STEP_TYPE_CONTRACT_DESTRUCT = 'contractDestruct'
-STEP_TYPE_CONTRACT_SET = 'contractSet'
-STEP_TYPE_SET = 'set'
-STEP_TYPE_REPLACE = 'replace'
-STEP_TYPE_DELETE = 'delete'
-STEP_TYPE_INPUT = 'input'
-STEP_TYPE_EVENT_LOG = 'eventLog'
-VALID_STEP_COST_KEYS = [STEP_TYPE_DEFAULT,
-                        STEP_TYPE_CONTRACT_CALL, STEP_TYPE_CONTRACT_CREATE, STEP_TYPE_CONTRACT_UPDATE,
-                        STEP_TYPE_CONTRACT_DESTRUCT, STEP_TYPE_CONTRACT_SET,
-                        STEP_TYPE_SET, STEP_TYPE_REPLACE, STEP_TYPE_DELETE, STEP_TYPE_INPUT,
-                        STEP_TYPE_EVENT_LOG]
-
 
 class Governance(IconScoreBase):
 
     _SCORE_STATUS = 'score_status'
     _AUDITOR_LIST = 'auditor_list'
-    _DEPLOYER_LIST = 'deployer_list'
     _STEP_PRICE = 'step_price'
-    _STEP_COSTS = 'step_costs'
-    _MAX_STEP_LIMIT = 'max_step_limit'
+
+    # TODO: replace with real func
+    _MAP_ADDRESS = {
+        'e0f6dc6607aa9b5550cd1e6d57549f67fe9718654cde15258922d0f88ff58b27': 'cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32',
+        'e22222222222222250cd1e6d57549f67fe9718654cde15258922d0f88ff58b27': 'cx222222222f5b45bfaea8cff1d8232fbb6122ec32',
+    }
+    _MAP_TXHASH = {
+        'cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32': '0xe0f6dc6607aa9b5550cd1e6d57549f67fe9718654cde15258922d0f88ff58b27',
+        'cx222222222f5b45bfaea8cff1d8232fbb6122ec32': '0xe22222222222222250cd1e6d57549f67fe9718654cde15258922d0f88ff58b27',
+    }
 
     @eventlog(indexed=1)
     def Accepted(self, tx_hash: str):
         pass
 
     @eventlog(indexed=1)
-    def Rejected(self, tx_hash: str, reason: str):
-        pass
-
-    @eventlog(indexed=1)
-    def StepPriceChanged(self, step_price: int):
-        pass
-
-    @eventlog(indexed=1)
-    def StepCostChanged(self, step_type: str, cost: int):
+    def Rejected(self, tx_hash: str):
         pass
 
     def __init__(self, db: IconScoreDatabase) -> None:
         super().__init__(db)
-        self._dummy_step = 0  # for TEST
         self._score_status = DictDB(self._SCORE_STATUS, db, value_type=bytes, depth=3)
         self._auditor_list = ArrayDB(self._AUDITOR_LIST, db, value_type=Address)
-        self._deployer_list = ArrayDB(self._DEPLOYER_LIST, db, value_type=Address)
         self._step_price = VarDB(self._STEP_PRICE, db, value_type=int)
-        self._step_costs = DictDB(self._STEP_COSTS, db, value_type=int)
-        self._max_step_limit = VarDB(self._MAX_STEP_LIMIT, db, value_type=int)
 
-    def on_install(self, stepPrice: int = 10 ** 12, maxStepLimit: int = 0x4000000) -> None:
+    def on_install(self, stepPrice: int = 10 ** 12) -> None:
         super().on_install()
         # add owner into initial auditor list
-        Logger.debug(f'on_install: owner = "{self.owner}"', TAG)
         self._auditor_list.put(self.owner)
-        # add owner into initial deployer list
-        self._deployer_list.put(self.owner)
         # set initial step price
         self._step_price.set(stepPrice)
-        # set initial step costs
-        self._set_initial_step_costs()
-        # set initial max step limit
-        self._max_step_limit.set(maxStepLimit)
 
     def on_update(self) -> None:
         super().on_update()
@@ -101,7 +72,7 @@ class Governance(IconScoreBase):
                 if key == STATUS:
                     status[key] = value.decode()
                 else:
-                    status[key] = value
+                    status[key] = '0x' + value.hex()
                 count += 1
         return count, status
 
@@ -122,40 +93,26 @@ class Governance(IconScoreBase):
     @external(readonly=True)
     def getScoreStatus(self, address: Address) -> dict:
         # check score address
-        current_tx_hash, next_tx_hash = self.get_tx_hashes_by_score_address(address)
-        if current_tx_hash is None and next_tx_hash is None:
+        # TODO: replace with real func
+        tx_hash: str = None
+        if str(address) in self._MAP_TXHASH:
+            tx_hash = self._MAP_TXHASH[str(address)]
+        else:
             self.revert('SCORE not found')
         result = {}
-        build_initial_status = False
-        # get current status
         _current = self._get_current_status(address)
         count1, status = self._fill_status_with_str(_current)
         if count1 > 0:
-            if current_tx_hash is None:
-                self.revert('current_tx_hash is None')
-            if current_tx_hash != status[DEPLOY_TX_HASH]:
-                self.revert('Current deploy tx mismatch')
-            # audit has been performed (accepted)
             result[CURRENT] = status
-        # get next status
         _next = self._get_next_status(address)
         count2, status = self._fill_status_with_str(_next)
         if count2 > 0:
-            # check if another pending tx has been arrived
-            if next_tx_hash is not None and \
-                    next_tx_hash != status[DEPLOY_TX_HASH]:
-                build_initial_status = True
-            else:
-                # audit has been performed (rejected)
-                result[NEXT] = status
-        else:
-            if next_tx_hash is not None:
-                build_initial_status = True
-        # there is no information, build initial status
-        if build_initial_status:
+            result[NEXT] = status
+        if count1 + count2 == 0:
+            # there is no status information, build initial status
             status = {
                 STATUS: STATUS_PENDING,
-                DEPLOY_TX_HASH: next_tx_hash
+                DEPLOY_TX_HASH: tx_hash
             }
             result[NEXT] = status
         return result
@@ -165,23 +122,18 @@ class Governance(IconScoreBase):
         return self._step_price.get()
 
     @external
-    def setStepPrice(self, stepPrice: int):
-        # only owner can set new step price
-        if self.msg.sender != self.owner:
-            self.revert('Invalid sender: not owner')
-        if stepPrice > 0:
-            self._step_price.set(stepPrice)
-            self.StepPriceChanged(stepPrice)
-
-    @external
     def acceptScore(self, txHash: bytes):
         # check message sender
         Logger.debug(f'acceptScore: msg.sender = "{self.msg.sender}"', TAG)
         if self.msg.sender not in self._auditor_list:
             self.revert('Invalid sender: no permission')
         # check txHash
-        score_address = self.get_score_address_by_tx_hash(txHash)
-        if score_address is None:
+        # TODO: replace with real func
+        score_address: Address = None
+        hex_string = txHash.hex()
+        if hex_string in self._MAP_ADDRESS:
+            score_address = Address.from_string(self._MAP_ADDRESS[hex_string])
+        else:
             self.revert('Invalid txHash')
         Logger.debug(f'acceptScore: score_address = "{score_address}"', TAG)
         # check next: it should be 'pending'
@@ -203,8 +155,7 @@ class Governance(IconScoreBase):
             AUDIT_TX_HASH: self.tx.hash
         }
         self._save_status(_current, status)
-        self.deploy_dummy(txHash)  # for TEST
-        self.Accepted('0x' + txHash.hex())
+        self.Accepted('0x' + hex_string)
 
     @external
     def rejectScore(self, txHash: bytes, reason: str):
@@ -213,8 +164,12 @@ class Governance(IconScoreBase):
         if self.msg.sender not in self._auditor_list:
             self.revert('Invalid sender: no permission')
         # check txHash
-        score_address = self.get_score_address_by_tx_hash(txHash)
-        if score_address is None:
+        # TODO: replace with real func
+        score_address: Address = None
+        hex_string = txHash.hex()
+        if hex_string in self._MAP_ADDRESS:
+            score_address = Address.from_string(self._MAP_ADDRESS[hex_string])
+        else:
             self.revert('Invalid txHash')
         Logger.debug(f'rejectScore: score_address = "{score_address}", reason = {reason}', TAG)
         # check next: it should be 'pending'
@@ -233,7 +188,7 @@ class Governance(IconScoreBase):
             AUDIT_TX_HASH: self.tx.hash
         }
         self._save_status(_next, status)
-        self.Rejected('0x' + txHash.hex(), reason)
+        self.Rejected('0x' + hex_string)
 
     @external
     def addAuditor(self, address: Address):
@@ -266,121 +221,3 @@ class Governance(IconScoreBase):
         Logger.debug(f'{header}: list len = {len(self._auditor_list)}', TAG)
         for auditor in self._auditor_list:
             Logger.debug(f' --- {auditor}', TAG)
-
-    @external
-    def addDeployer(self, address: Address):
-        # check message sender, only owner can add new deployer
-        if self.msg.sender != self.owner:
-            self.revert('Invalid sender: not owner')
-        if address not in self._deployer_list:
-            self._deployer_list.put(address)
-        if DEBUG is True:
-            self._print_deployer_list('addDeployer')
-
-    @external
-    def removeDeployer(self, address: Address):
-        if address not in self._deployer_list:
-            self.revert('Invalid address: not in list')
-        # check message sender
-        if self.msg.sender != self.owner:
-            if self.msg.sender != address:
-                self.revert('Invalid sender: not yourself')
-        # get the topmost value
-        top = self._deployer_list.pop()
-        if top != address:
-            for i in range(len(self._deployer_list)):
-                if self._deployer_list[i] == address:
-                    self._deployer_list[i] = top
-        if DEBUG is True:
-            self._print_deployer_list('removeDeployer')
-
-    @external(readonly=True)
-    def isDeployer(self, address: Address) -> bool:
-        Logger.debug(f'isDeployer address: {address}', TAG)
-        return address in self._deployer_list
-
-    def _print_deployer_list(self, header: str):
-        Logger.debug(f'{header}: list len = {len(self._deployer_list)}', TAG)
-        for deployer in self._deployer_list:
-            Logger.debug(f' --- {deployer}', TAG)
-
-    def _set_initial_step_costs(self):
-        initial_costs = {
-            STEP_TYPE_DEFAULT: 4000,
-            STEP_TYPE_CONTRACT_CALL: 1500,
-            STEP_TYPE_CONTRACT_CREATE: 20000,
-            STEP_TYPE_CONTRACT_UPDATE: 8000,
-            STEP_TYPE_CONTRACT_DESTRUCT: -7000,
-            STEP_TYPE_CONTRACT_SET: 1000,
-            STEP_TYPE_SET: 20,
-            STEP_TYPE_REPLACE: 5,
-            STEP_TYPE_DELETE: -15,
-            STEP_TYPE_INPUT: 20,
-            STEP_TYPE_EVENT_LOG: 10
-        }
-        for key in VALID_STEP_COST_KEYS:
-            value: int = initial_costs[key]
-            self._step_costs[key] = value
-
-    @external(readonly=True)
-    def getStepCosts(self) -> dict:
-        result = {}
-        for key in VALID_STEP_COST_KEYS:
-            result[key] = self._step_costs[key]
-        return result
-
-    @external
-    def setStepCost(self, stepType: str, cost: int):
-        # only owner can set new step cost
-        if self.msg.sender != self.owner:
-            self.revert('Invalid sender: not owner')
-        if stepType not in VALID_STEP_COST_KEYS:
-            self.revert(f'Invalid step type: {stepType}')
-        if cost < 0:
-            if stepType != STEP_TYPE_CONTRACT_DESTRUCT and \
-                    stepType != STEP_TYPE_DELETE:
-                self.revert(f'Invalid step cost: {stepType}, {cost}')
-        self._step_costs[stepType] = cost
-        self.StepCostChanged(stepType, cost)
-
-    @external(readonly=True)
-    def getMaxStepLimit(self) -> int:
-        return self._max_step_limit.get()
-
-    def get_tx_hashes_by_score_address(self, address):
-        _MAP_VALID_TXHASH1 = {
-            'cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32': 'e0f6dc6607aa9b5550cd1e6d57549f67fe9718654cde15258922d0f88ff58b27'
-        }
-        _MAP_VALID_TXHASH2 = {
-            'cx222222222f5b45bfaea8cff1d8232fbb6122ec32': 'e22222222222222250cd1e6d57549f67fe9718654cde15258922d0f88ff58b27'
-        }
-        _current = None
-        _next = None
-        print(f' ===== _dummy_step = {self._dummy_step}')
-        if str(address) in _MAP_VALID_TXHASH1:
-            tx_hash = _MAP_VALID_TXHASH1[str(address)]
-            if self._dummy_step == 0:
-                _next = bytes.fromhex(tx_hash)
-            elif self._dummy_step == 1 or self._dummy_step == 2:
-                _current = bytes.fromhex(tx_hash)
-                self.deploy_dummy(tx_hash)
-            elif self._dummy_step == 3:
-                _current = bytes.fromhex(tx_hash)
-                _next = bytes.fromhex('3' * 40)
-        elif str(address) in _MAP_VALID_TXHASH2:
-            tx_hash = _MAP_VALID_TXHASH2[str(address)]
-            _next = bytes.fromhex(tx_hash)
-        return _current, _next
-
-    def get_score_address_by_tx_hash(self, tx_hash):
-        _MAP_ADDRESS = {
-            'e0f6dc6607aa9b5550cd1e6d57549f67fe9718654cde15258922d0f88ff58b27': 'cxb0776ee37f5b45bfaea8cff1d8232fbb6122ec32',
-            'e22222222222222250cd1e6d57549f67fe9718654cde15258922d0f88ff58b27': 'cx222222222f5b45bfaea8cff1d8232fbb6122ec32',
-        }
-        hex_string = tx_hash.hex()
-        if hex_string in _MAP_ADDRESS:
-            return Address.from_string(_MAP_ADDRESS[hex_string])
-        return
-
-    def deploy_dummy(self, tx_hash):
-        self._dummy_step += 1
